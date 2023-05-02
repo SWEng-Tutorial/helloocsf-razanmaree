2ca11e1222ff237ede7a26164356a05146aaaeec
razanmaree <101133266+razanmaree@users.noreply.github.com>
2023-05-02 12:55:13 +0300

    first

--- client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/SimpleClient.java
+++ client/src/main/java/il/cshaifasweng/OCSFMediatorExample/client/SimpleClient.java
@@ -29,7 +29,7 @@ public class SimpleClient extends AbstractClient {
 	
 	public static SimpleClient getClient() {
 		if (client == null) {
-			client = new SimpleClient("localhost", 3000);
+			client = new SimpleClient("192.168.36.230", 3000);
 		}
 		return client;
 	}
